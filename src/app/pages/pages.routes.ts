854cf95090037f6c9b979bc6f32e6faf497007cb
Jose Jeanton <jajeantp@gmail.com>
2019-03-20 16:40:28 -0600

    Seccion 13 terminada

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -6,6 +6,7 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { LoginGuardGuard } from '../services/service.index';
 
 
 
@@ -13,6 +14,7 @@ const pagesRoute: Routes = [
     {
         path: '',
         component: PagesComponent,
+        canActivate: [LoginGuardGuard],
         children: [
             { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
             { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
